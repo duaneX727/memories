ad0b5ddc8c99017bfcbda9a93c6ce28a811f4faa
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-07 18:27:01 -0400

    Added Routing to server.

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ linit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ linit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://mdmitch7:07032018911@cluster0.j0tc6hp.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5060;
+const PORT = process.env.PORT || 3060;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
