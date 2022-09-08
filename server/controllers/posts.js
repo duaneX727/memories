4642d5cbd72bc258b591fec5953649a7d9d4f393
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-07 21:15:33 -0400

    Added CreatePost functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,6 +8,15 @@ export const getPosts = async (req, res) => {
     res.status(404).json({message: err.message});
   }
 }
-export const createPost = (req, res) => {
-  res.send('Post Creation');
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+  } catch (err) {
+    res.status(409).json({
+      message: err.message
+    });
+  }
 }
