63e50ef0aa89fa5ff26048444788035f68f0f115
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-10 00:32:53 -0400

    -Added CREATE functionality to the reducer -Added newPosts to Form.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,13 @@
-export default (posts = [], action) => {
-    switch (action.type) {
-      case 'FETCH_ALL':
-        return action.payload;
-      case 'CREATE': 
-        return posts;
-      default:
-        return posts;
-    }
+export default(posts = [], action) => {
+  switch (action.type) {
+    case 'FETCH_ALL':
+      return action.payload;
+    case 'CREATE':
+      return [
+        ...posts,
+        action.payload
+      ];
+    default:
+      return posts;
+  }
 }
