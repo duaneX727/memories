6ca588c0f16fb3e68e0859a8ae4cc24958edf598
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-26 23:17:54 -0400

    Typo post.id to post._id

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -8,7 +8,7 @@ export default(posts = [], action) => {
         action.payload
       ];
       case 'UPDATE':
-      return posts.map( post => post.id === action.payload._id ? action.payload : post);
+      return posts.map( post => post._id === action.payload._id ? action.payload : post);
     default:
       return posts;
   }
