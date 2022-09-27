a220195b0864882506f4a7e033de7b8400b9eeab
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-26 22:42:48 -0400

    fontSize from default to medium

--- server/index.js
+++ server/index.js
@@ -8,24 +8,22 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
 config();
-//const CONNECTION_URL = process.env.DB_URI;
+// const CONNECTION_URL = process.env.DB_URI;
 
 const PORT = process.env.PORT || 3060;
 
 console.log('Connecting to MongoDB Atlas cluster...');
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Successfully connected to MongoDB Atlas!
-    Server running on port: ${PORT}`)
-    ))
-    .catch((err) => {
-        console.error('Connection to MongoDB Atlas failed!', err);
-        process.exit();
-    });
-
-
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Successfully connected to MongoDB Atlas!
+    Server running on port: ${PORT}`))).catch((err) => {
+  console.log('Connection to MongoDB Atlas failed!', err);
+  process.exit();
+});
