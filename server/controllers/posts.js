08a0ddb065642869608d6fd086f3286ccb39039c
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-29 16:23:57 -0400

    Major correction on styles.js in root

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,13 +13,9 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body;
   const newPost = new PostMessage(post);
-
   try {
-
     await newPost.save();
-
     res.status(200).json(newPost);
-
   } catch (err) {
     res.status(409).json({message: err.message});
   }
