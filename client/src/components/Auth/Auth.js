60ae9cd155ea4507fbe9faac0ecb73ea75ffab4c
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-30 17:15:26 -0400

    Added a toggle to switch login modes

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,47 +1,108 @@
 import React, {useState} from 'react';
-import { Avatar, Button, Paper,Grid,Typography,Container, TextField } from '@material-ui/core';
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container
+} from '@material-ui/core';
 import useStyles from './styles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Input from './Input';
 const Auth = () => {
-  const isSignUp = false;
   const classes = useStyles();
-  const [showPassword,setShowPassword] = useState(false);
-  const handleShowPassword = () => {
-
-  }
-  const handleSubmit = () => setShowPassword(prevShowPassword => !prevShowPassword);
-
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
   
-  const handleChange = () => {
 
-  }
+  const switchMode = () => {
+    setIsSignUp(prevIsSignUp => !prevIsSignUp);
+    handleShowPassword(false);
+  };
+
+  const handleShowPassword = () => setShowPassword(prevShowPassword => !prevShowPassword);
+
+
+  const handleSubmit = () => {}
+
+
+  const handleChange = () => {}
   return (
     <Container component="main" maxWidth="xs">
-      <Paper className={classes.paper}elevation={3}>
-        <Avatar className={classes.avatar}>
+      <Paper className={
+          classes.paper
+        }
+        elevation={3}>
+        <Avatar className={
+          classes.avatar
+        }>
           <LockOutlinedIcon/>
         </Avatar>
         <Typography variant="h5">
-          {isSignUp ? "Sign Up":"Sign In"}
+          {
+          isSignUp ? "Sign Up" : "Sign In"
+          } 
         </Typography>
-        <form  className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
+
+
+        <form className={
+            classes.form
+          }
+          onSubmit={handleSubmit}>
+          <Grid container
+            spacing={2}>
             {
-              isSignUp && (
-                <>
-                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half/>
-                    <Input name="lastName" label="Last Name" handleChange={handleChange} half/>
-                </>
-            )}
-              <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
-              <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
-              { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+            isSignUp && (
+              <>
+                <Input name="firstName" label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half/>
+                <Input name="lastName" label="Last Name"
+                  handleChange={handleChange}
+                  half/>
+              </>
+            )
+          }
+            <Input name="email" label="Email Address"
+              handleChange={handleChange}
+              type="email"/>
+            <Input name="password" label="Password"
+              handleChange={handleChange}
+              type={
+                showPassword ? "text" : "password"
+              }
+              handleShowPassword={handleShowPassword}/> {
+            isSignUp && <Input name="confirmPassword" label="Repeat Password"
+              handleChange={handleChange}
+              type="password"/>
+            }
+           </Grid>
+          <Button className={
+              classes.submit
+            }
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary">
+            {
+            isSignUp ? 'Sign Up' : 'Sign In'
+          } </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Button onClick={switchMode}>
+                {
+                isSignUp ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"
+              }
+              </Button>
+            </Grid>
           </Grid>
         </form>
       </Paper>
     </Container>
-  )
-}
+  );
+};
 
-export default Auth
+
+export default Auth;
