70f6b7732e83c2ad61bda43ac2e9020ef21e39af
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-29 21:54:18 -0400

    installed http-proxy-middleware

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,5 @@
 import {FETCH_ALL,CREATE,UPDATE,DELETE} from '../constants/actionTypes'
-export default(posts = [], action) => {
+export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
@@ -9,7 +9,7 @@ export default(posts = [], action) => {
         action.payload
       ];
     case UPDATE:
-      return posts.map( post => post._id === action.payload ? action.payload : post);
+      return posts.map( post => post._id === action.payload._id ? action.payload : post);
     case DELETE:
       return posts.filter(post => post._id !== action.payload);
     default:
