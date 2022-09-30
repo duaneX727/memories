485e7499cb2851fc89476926795a03b36f2cea1d
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-29 23:48:58 -0400

    Installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import {Container, Grow, Grid} from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar'
 import { useDispatch } from 'react-redux';
 import {getPosts} from './actions/posts';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import memories from "./images/memories.png";
+
 import useStyles from './styles';
 
 const App = () => {
@@ -17,15 +18,10 @@ const App = () => {
   }, [currentId, dispatch]);
   return (
     <Container maxwidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-          <img className={classes.image} src={memories} alt="memories" height="60" />
-        </Typography>
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
-          <Grid container
+          <Grid className={classes.mainContainer} container
             justifyContent="space-between" alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}/>
